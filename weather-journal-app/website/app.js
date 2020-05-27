9d197dcb885ca851d11beda2287c662e868e0713
btsixer <60943928+btsixer@users.noreply.github.com>
2020-05-27 13:31:07 -0500

    Add files via upload

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -5,12 +5,26 @@ let apiKey = '0a97adefbee609e683f0320feb8254c2';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
+let newDate = 'Today is '+n+' '+ d.getDate()+', '+ d.getFullYear();
 
 // Convert temperature in kelvin to Fahrenheits
 function kelvinToFahrenheit(k){
     const f = ((k-273.15)*1.8)+32;
-    return f.toFixed(2);
+    return f.toFixed(0);
 }
 
 
@@ -20,8 +34,8 @@ const getWeather = async (baseURL, zipCode, apiKey)=>{
   const res = await fetch(`${baseURL}zip=${zipCode},us&appid=${apiKey}`)
   try {
     const projectData = await res.json();
-    console.log(projectData);
-    console.log(projectData.main['temp']);
+    // console.log(projectData);
+    // console.log(projectData.main['temp']);
     return projectData;
   }  catch(error) {
     console.log('error', error);
@@ -31,7 +45,7 @@ const getWeather = async (baseURL, zipCode, apiKey)=>{
 // Write another async function that is called after the completed POST request.
 const postWeather = async(url = '', data = {}) => {
   // console.log('postData', projectData);
-  console.log(data);
+  // console.log(data);
   const response = await fetch(url,{
       method: 'POST',
       credentials: 'same-origin',
@@ -44,7 +58,7 @@ const postWeather = async(url = '', data = {}) => {
       // const newData = await res.json();
       // console.log('postData', newData);
       // return newData;
-      console.log('POST test');
+      // console.log('POST test');
   }catch (error) {
       console.log('error', error);
   }
@@ -67,24 +81,19 @@ function performAction(e){
      postWeather('/addData', {temperature: data.main.temp, date: newDate, userResponse: userResponse})
     })
     .then (() => updateUI());;
-    // postWeather('', {
-    //             temperature: kelvinToFahrenheit(projectData.main['temp']),
-    //             date: d,
-    //             userResponse: userResponse
-    //         });
 };
 
 //Upddate UI
 const updateUI = async () => {
     const request = await fetch('/all')
-
     try{
         const entryData = await request.json()
         JSON.stringify(entryData);
         console.log(entryData);
-    document.getElementById('date').innerHTML = entryData[0].date;
-    document.getElementById('temp').innerHTML = entryData[0].main.temp;
-    document.getElementById('content').innerHTML = entryData[0].userResponse;
+        const tempFar = kelvinToFahrenheit(entryData.temperature);
+    document.getElementById('date').innerHTML = entryData.date;
+    document.getElementById('temp').innerHTML = 'The temperature right now is '+tempFar+' degrees Fahrenheit.';
+    document.getElementById('content').innerHTML = entryData.userResponse;
 
     }
     catch(error) {
